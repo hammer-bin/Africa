8c141419afed7e24599c31787d43a29735bd96e0
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-08-12 06:07:16 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by minkyuLee on 2022/08/12.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
